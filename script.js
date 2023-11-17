b72cfcef29b215b135a473f1c8262ec3a962c661
Evgeny Darkhanov <zhdarxanov@gmail.com>
2023-11-18 02:49:09 +0300

    fix math, wrap in class

--- script.js
+++ script.js
@@ -1,73 +1,92 @@
+class GradientCircle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        this.t = 0;
+        this.div_array = GradientCircle.create_div_array(this.width, this.height);
+    }
 
-let width = 100;
-let height = 100;
-let aspect = width/height;
-let gradient = ['#000000','#505050','#808080','#BABABA','#FFFFFF']
+    static init_matrix(height,width)
+    {
+        let arr = new Array(width);
+        for (let i = 0; i < width; i++){
+            arr[i] = new Array (height);
+        }
+        return arr;
+    }
 
-let arr = new Array(width)
-for (let i = 0; i < width;i++){
-    arr[i] = new Array (height)
-}
+    static create_div_array(height, width)
+    {
+        let arr = GradientCircle.init_matrix(height, width);
+        for(let i = 0; i<height; i++){
+            for(let j=0; j<width; j++){
+                let div = document.createElement('div');
+                div.style.width = '5px';
+                div.style.height = '5px';
+                div.style.background = 'white';
+                div.style.position = 'absolute';
+                div.style.left = 350+j*5+'px';
+                div.style.top = i*5+'px';
+                arr[i][j] = div;
+                document.body.appendChild(div);
+            }
+        }
+        return arr;
+    }
 
+    static calc_distance(x1,y1,x2,y2)
+    {
+        return Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2))
+    }
 
+    static calc_point_position(t, center_x, center_y, radius)
+    {
+        let x = Math.cos(t*0.01)*radius+center_x;
+        let y = Math.sin(t*0.01)*radius+center_y;
+        return [x,y];
+    }
 
-console.log(arr);
+    static calc_color(distance, gradient, circle_radius=20)
+    {
+        let temp = 1/distance*circle_radius;
+        let color_index = Math.round(temp);
+        if(color_index<0) {color_index=0;}
+        if(color_index>gradient.length-1){color_index = gradient.length-1;}
+        return gradient[color_index] 
 
-    for(let i = 0; i<height;i++){
-        for(let j=0;j<width;j++){
-            
-            div = document.createElement('div');
-            arr[i][j] = div;
+    }
 
-            div.style.width = '5px';
-            div.style.height = '5px';
-            div.style.background = 'white';
-            //div.style.border = '1px solid black';
-            //div.style.display = 'inline-block';
-            div.style.position = 'absolute';
-            div.style.left = 350+j*5+'px';
-            div.style.top = i*5+'px';
-
-            document.body.appendChild(div);
-
-            
+    draw_gradient(x, y) 
+    {
+        const gradient = ['#000000','#505050','#808080','#BABABA','#FFFFFF']
+        for(let i = 0; i<this.height; i++){
+            for(let j=0; j<this.width; j++){
+                let color = GradientCircle.calc_color(GradientCircle.calc_distance(x,y, i, j), gradient)
+                this.div_array[i][j].style.background = color;
+            }
         }
     }
-let t=0;
 
-function circle(){
-for(let i = 0; i<height;i++){
-    for(let j=0;j<width;j++){
-
-
-        let x = j/width*2-1;
-        x = x*aspect;
-        x = x + Math.cos(t*0.000001);
-       
-
-        let y = i/height*2-1;
-        y = y + Math.sin(t*0.000001);
-
-        let dist = Math.sqrt(x*x+y*y);
-        let color = 1/dist;
-        color = Math.round(color);
-        if(color<0){color=0;}
-        if(color>gradient.length-1){color = gradient.length-1;}
-        
-
-        // if(x*x+y*y<0.7){
-        //     arr[i][j].style.background = gradient[color];
-        // }
-        // else{
-        //     arr[i][j].style.background = 'white';
-        // }
-        t = t+1;
-        arr[i][j].style.background = gradient[color];
-        
-        }
+    next_frame()
+    {
+        let min_from_h_w = Math.min.apply(Math, [this.height, this.width]);
+        const [x,y] = GradientCircle.calc_point_position(this.t, this.width/2, this.height/2, min_from_h_w/2);
+        this.draw_gradient(x,y);
+        this.t = this.t+1;
     }
 }
 
+function main()
+{
+    let width = 100;
+    let height = 100;
+    const gradient_circle = new GradientCircle(height,width);
+    function wrapper()
+    {
+        gradient_circle.next_frame();
+    }
+    
+    let play = setInterval(wrapper, 10);
+}
 
-//circle(arr);
-let play = setInterval(circle, 1);
+main();
